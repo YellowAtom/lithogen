3aae7e70afbca704c84979c78e02e6d7efc01f3f
YellowAtom <yellowatom2@proton.me>
2025-01-28 18:33:04 +0000

    Fix typo in frag shader

--- app/shaders/fragment.glsl
+++ app/shaders/fragment.glsl
@@ -1,8 +1,8 @@
 #version 460 core
 
-layout (location = 0) in vec3 flag_color;
+layout (location = 0) in vec3 frag_color;
 out vec4 out_color;
 
 void main() {
-	out_color = vec4(flag_color, 1.0f);
+	out_color = vec4(frag_color, 1.0f);
 }
