cab6ad2b85a1eb0edb194f13ab139e558070d9ce
YellowAtom <yellowatom2@proton.me>
2025-05-25 23:24:23 +0100

    Move GLFW callbacks to separate file

---
+++ src/control.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "config.h"
+#include "render.h"
+
+struct glfwUserData {
+	bool cursorWithinViewport = false;
+
+	Config* config;
+	Render* render;
+
+	glfwUserData(Config* config, Render* render) : config(config), render(render) {}
+};
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void CursorPosCallback(GLFWwindow* window, double x, double y);
+void ScrollCallback(GLFWwindow* window, double x, double y);
--- src/main.cpp
+++ src/main.cpp
@@ -8,82 +8,10 @@
 #include <numeric>
 #include "config.h"
 #include "constants.h"
+#include "control.h"
 #include "image.h"
 #include "interface.h"
 #include "render.h"
-
-// GLFW callbacks.
-
-struct glfwUserData {
-	bool cursorWithinViewport = false;
-
-	Config* config;
-	Render* render;
-
-	glfwUserData(Config* config, Render* render) : config(config), render(render) {}
-};
-
-void FramebufferSizeCallback(GLFWwindow* window, const int width, const int height) {
-	const auto* data = static_cast<glfwUserData*>(glfwGetWindowUserPointer(window));
-	// Recalculate viewport size when the window is resized.
-	data->render->CalcViewport(width, height);
-}
-
-void KeyCallback(GLFWwindow* window, const int key, const int scancode, const int action, const int mods) {
-	const auto* data = static_cast<glfwUserData*>(glfwGetWindowUserPointer(window));
-
-	if (!data->render->entity.HasModel()) {
-		return;
-	}
-
-	if (key == GLFW_KEY_R && action == GLFW_PRESS) {
-		data->render->camera.ResetRotation();
-	} else if (key == GLFW_KEY_W && action == GLFW_PRESS) {
-		data->config->drawWireframe = !data->config->drawWireframe;
-		data->render->UpdateWireframe();
-	}
-}
-
-void CursorPosCallback(GLFWwindow* window, const double x, const double y) {
-	auto* data = static_cast<glfwUserData*>(glfwGetWindowUserPointer(window));
-
-	if (!data->render->entity.HasModel()) {
-		return;
-	}
-
-	// TODO: Rotating the cube too much will make this inaccurate, find a way.
-
-	// Store previous mouse location to calculate difference.
-	// Static allows a variable to persist through multiple function calls.
-	static double previousX = 0;
-	static double previousY = 0;
-
-	int width = 0;
-	int height = 0;
-	glfwGetWindowSize(window, &width, &height);
-
-	data->cursorWithinViewport =
-		width - x < data->render->GetViewportWidth() && height - y < data->render->GetViewportHeight();
-
-	// Is left mouse down and is the cursor within the viewport.
-	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GL_TRUE && data->cursorWithinViewport) {
-		// Apply the difference between the previous and current mouse location to the camera's rotation.
-		data->render->camera.RotateHorizontal((x - previousX) / 20);
-		data->render->camera.RotateVertical((y - previousY) / 20);
-	}
-
-	previousX = x;
-	previousY = y;
-}
-
-void ScrollCallback(GLFWwindow* window, const double x, const double y) {
-	const auto* data = static_cast<glfwUserData*>(glfwGetWindowUserPointer(window));
-
-	// Move 4mm per scroll.
-	if (data->render->entity.HasModel() && data->cursorWithinViewport) {
-		data->render->camera.Zoom(y * 4);
-	}
-}
 
 int main(int argc, char* argv[]) {
 	// Initialize the GLFW3 library.
